0de2ca6d323a34978fe203f7891e99d683e2f546
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2024-04-08 10:06:49 +0200

    update non nullable

--- CurrencyConverter/ConversionDto.cs
+++ CurrencyConverter/ConversionDto.cs
@@ -3,8 +3,8 @@ namespace CurrencyConverter;
 public class ConversionDto
 {
   
-    public string? Source { get; set; }
-    public string? Target { get; set; }
+    public string Source { get; set; }
+    public string Target { get; set; }
     public float Value { get; set; }
     public float Result { get; set; }
 }
--- CurrencyConverter/History.cs
+++ CurrencyConverter/History.cs
@@ -5,8 +5,8 @@ namespace CurrencyConverter;
 public class History
 {
   public DateOnly ConversionDate { get; set; } 
-  public string? Source { get; set; }
-  public string? Target { get; set; }
+  public string Source { get; set; }
+  public string Target { get; set; }
   public float Value { get; set; }
   public float Result { get; set; }
 }
