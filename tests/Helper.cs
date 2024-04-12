6207b70c754515fcd706d3efcf612452bf27996b
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2024-04-12 17:23:34 +0200

    remove rebuild db

--- tests/Helper.cs
+++ tests/Helper.cs
@@ -17,32 +17,7 @@ public class Helper
         }
     }
 
-
-    public static void TriggerRebuild()
-    {
-        using var connection = new MySqlConnection("Server=localhost;Database=conversion_history;Uid=myuser;Pwd=mypassword;");
-        connection.Open();
-        try
-        {
-            connection.Execute("DROP TABLE IF EXISTS conversion_history.history;");
-                
-            connection.Execute(@"
-                
-                CREATE TABLE conversion_history.history (
-                    id INT AUTO_INCREMENT PRIMARY KEY,
-                    date DATE DEFAULT CURRENT_DATE() NOT NULL,
-                    source TEXT NOT NULL,
-                    target TEXT NOT NULL,
-                    value FLOAT NOT NULL,
-                    result FLOAT NOT NULL
-                );");
-        }
-        catch (Exception e)
-        {
-            throw new Exception($"There was an error rebuilding the database.", e);
-        }
-    }
-
+    
     public static string GetConnectionString()
     {
         return ConnectionString;
